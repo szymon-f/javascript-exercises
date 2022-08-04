1e3e784cd6d244aac356fcf65b48451efd355c03
szymon-f <sfluder25@gmail.com>
2022-08-04 13:44:34 +0200

    complete js exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    // let titles = [];
+    // for(const b of books){
+    //     titles.push(b.title);
+    // }
+    // return titles;
+    return books.map(book => book.title);
 };
 
 // Do not edit below this line

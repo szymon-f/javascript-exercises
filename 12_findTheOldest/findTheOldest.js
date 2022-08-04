ced844a4163b79b32c10e3d903f19a98a0ceeaea
szymon-f <sfluder25@gmail.com>
2022-08-04 14:06:33 +0200

    complete js exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestPeople = people.sort((a, b) => {
+        if(!a.yearOfDeath){
+            a.yearOfDeath = new Date().getFullYear()
+        }
+        if(!b.yearOfDeath){
+            b.yearOfDeath = new Date().getFullYear()
+        }
+        let aLifespan = a.yearOfDeath - a.yearOfBirth;
+        let bLifespan = b.yearOfDeath - b.yearOfBirth;
+        return aLifespan > bLifespan ? 1 : -1;
+    })
+    return oldestPeople[oldestPeople.length - 1];
 };
 
 // Do not edit below this line

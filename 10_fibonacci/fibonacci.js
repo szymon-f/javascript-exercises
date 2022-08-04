5050e97a184f30def4809dcedf0a72ba6e1147b9
szymon-f <sfluder25@gmail.com>
2022-08-04 13:36:47 +0200

    complete js exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n < 0){
+        return "OOPS";
+    }
+    if(typeof(n) === 'string'){
+        n = +n;
+    }
+    let fib_nums = [0, 1];
+    let iter = [...Array(n + 1).keys()].splice(2);
+    for(const i of iter){
+        fib_nums.push(fib_nums[i - 1] + fib_nums[i - 2])
+    }
+    return fib_nums[n];
 };
 
 // Do not edit below this line

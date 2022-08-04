81b7073c94ebc95133cd9a5c429cb977f431a8c9
szymon-f <sfluder25@gmail.com>
2022-08-04 13:17:01 +0200

    complete js exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+    sentence = sentence.toLowerCase().replace(/[' ']/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    return sentence === sentence.split('').reverse().join('');
 };
 
 // Do not edit below this line

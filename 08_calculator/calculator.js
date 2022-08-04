8b0fb4ed1ec37a1f0b889025718c9cbd0585cd03
szymon-f <sfluder25@gmail.com>
2022-08-04 12:57:17 +0200

    complete js exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b ) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let sum = numbers.reduce((total, num) => {
+    return total + num;
+  }, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let mult = numbers.reduce((total, num) => {
+    return total * num;
+  }, 1);
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0){
+    return 1;
+  }
+  let nums = [...Array(num + 1).keys()];
+  nums.shift();
+  return nums.reduce((total, num) => {
+    return total * num;
+  })
 };
 
 // Do not edit below this line
